d086ccaebed0a793222a8f0a1a744a2bdf9bfd73
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-19 17:38:55 +0300

    Create Role.java

---
+++ project-05-authentication-authorization-system/Role.java
@@ -0,0 +1,23 @@
+package com.alhanoof.security.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roleName;
+
+    public Role() {
+    }
+
+    public Role(String roleName) {
+        this.roleName = roleName;
+    }
+
+    // Getters and Setters
+}
