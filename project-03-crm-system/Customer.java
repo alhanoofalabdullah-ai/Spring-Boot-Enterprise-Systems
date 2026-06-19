56d1ba913fb1b23a70a854d488150b48049faf8d
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-19 17:22:54 +0300

    Create Customer.java

---
+++ project-03-crm-system/Customer.java
@@ -0,0 +1,88 @@
+package com.alhanoof.crm.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String phone;
+
+    private String company;
+
+    private String status;
+
+    public Customer() {
+    }
+
+    public Customer(
+            String fullName,
+            String email,
+            String phone,
+            String company,
+            String status
+    ) {
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.company = company;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
