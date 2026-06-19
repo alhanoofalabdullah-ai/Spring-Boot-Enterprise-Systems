2ceb4ad3bc1d57e6a21e54e82a4d2cf6205f8a1e
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-19 17:47:32 +0300

    Create ProductService.java

---
+++ project-06-ecommerce-backend-platform/ProductService.java
@@ -0,0 +1,25 @@
+package com.alhanoof.ecommerce.service;
+
+import com.alhanoof.ecommerce.entity.Product;
+import com.alhanoof.ecommerce.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository repository;
+
+    public ProductService(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+
+    public Product createProduct(Product product) {
+        return repository.save(product);
+    }
+}
