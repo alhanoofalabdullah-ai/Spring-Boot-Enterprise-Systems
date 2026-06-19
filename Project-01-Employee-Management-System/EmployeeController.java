b5f576451abaefe02f90c035077321ab2d1d09f8
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-19 17:10:08 +0300

    Create EmployeeController.java

---
+++ Project-01-Employee-Management-System/EmployeeController.java
@@ -0,0 +1,47 @@
+package com.example.employees.controller;
+
+import com.example.employees.entity.Employee;
+import com.example.employees.service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private final EmployeeService service;
+
+    public EmployeeController(EmployeeService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return service.getAllEmployees();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable Long id) {
+        return service.getEmployeeById(id);
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return service.createEmployee(employee);
+    }
+
+    @PutMapping("/{id}")
+    public Employee updateEmployee(
+            @PathVariable Long id,
+            @RequestBody Employee employee
+    ) {
+        return service.updateEmployee(id, employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteEmployee(@PathVariable Long id) {
+        service.deleteEmployee(id);
+        return "Employee deleted successfully";
+    }
+}
