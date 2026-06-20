af83edbc52d00d47e62c1fce506d19181b2d8e14
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-20 09:32:13 +0300

    Create PowerBIConnector.java

---
+++ project-20-enterprise-digital-transformation-platform/PowerBIConnector.java
@@ -0,0 +1,9 @@
+package com.alhanoof.digitaltransformation.integration;
+
+public class PowerBIConnector {
+
+    public String publishDashboard() {
+
+        return "Power BI Dashboard Published";
+    }
+}
